198c7ac878f5261802450d5a6d22221a80680fb6
elijahbeminiyi <elijahbeminiyi@gmail.com>
2022-10-17 17:55:06 +0100

    some codes written

--- main.h
+++ main.h
@@ -1,14 +1,43 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdout.h>
 
 int _printf(const char *format, ...);
 
 int _printf(const char *format, ...)
 {
 	/* Declarations */
-
+	va_list ap;
+	char *tmp, *sval;
 
 	/* Statements */
+	va_start(ap, format);
+	for(p = fmt; *p; p++)
+	{
+		if (*p != "%")
+			putchar(p);
+			continue;
+
+		switch (++p)
+		{
+			case 'c':
+				for (sval = va_arg(ap, char *); *sval; sval++)
+            		putchar(*sval);
+            		continue;
+            	break;
+            case 's':
+				for (sval = va_arg(ap, char *); *sval; sval++)
+            		putchar(*sval);
+            	break;
+            case '%':
+            	putchar('%');
+            	break;
+            default:
+            	putchar('\n');
+            	break;
+		}
+	}
 }
 
 #endif
